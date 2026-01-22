e1c6c827f2f5057db6aa8b459af159a576384530
Gustavo Lopes <guhrlopes999@gmail.com>
2026-01-21 22:37:12 -0300

    Alterada opções de uso da API, dados sendo carregados

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const endpoint = "https://api.hgbrasil.com/weather?key=";
 const apiKey = "b94f2b54";
 
+const umidade = document.getElementById("umidade");
+const ventos = document.getElementById("ventos");
+const nascerDoSOl = document.getElementById("nascerDoSol");
+const porDoSol = document.getElementById("porDoSol");
+const chanceDeChuva = document.getElementById("chanceDeChuva");
+const temperatura = document.getElementById("temperatura")
+
+const city = document.getElementById("city");
+const dateTime = document.getElementById("dateTime"); 
+const descricao = document.getElementById("descricao");
+
 const input = document.getElementById("inputLocal");
 const button = document.getElementById("searchButton");
 
@@ -10,4 +21,25 @@ button.addEventListener("click", () => {
 
   let url = endpoint + apiKey + "&city_name="+ cityName;
 
+  fetch(url)
+  .then(response => response.json())
+  .then(dados => {
+    temperatura.textContent = dados.results.temp;
+
+    city.textContent = dados.results.city;
+
+    dateTime.textContent = dados.results.date;
+
+    descricao.textContent = dados.results.description;
+
+    umidade.textContent = dados.results.humidity;
+
+    ventos.textContent = dados.results.wind_speedy;
+
+    nascerDoSOl.textContent = dados.results.sunrise;
+
+    porDoSol.textContent = dados.results.sunset;
+
+    chanceDeChuva.textContent = dados.results.rain;
+  })
 })
